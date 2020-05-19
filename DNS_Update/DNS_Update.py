22b60682ff145df869e68dcb96658fd108f28adc
Robert Jensen <robert@robert-jensen.dk>
2020-05-19 09:46:53 +0200

    Clean up code

--- DNS_Update/DNS_Update.py
+++ DNS_Update/DNS_Update.py
@@ -4,20 +4,18 @@ import winrm
 event         = ""                                          #Provision or remove.
 ipadress      = ""                                          #Cleaned up IP
 hostname      = ""                                          #Cleaned up Hostname
-requestid     = ""                                          #Custom input value, containing Request id. Being  set in custom field later
-project       = ""                                          #The project the deployment belongs to.
-owner         = ""                                          #The requester of the ressource
+DNS_Server    = ""                                          #DNS server where the command will be executed
+DNS_Domain    = ""                                          #The DNS Domain to be updated
+Username      = ""                                          #Username with right to do the operation
+Password      = ""                                          #Password for the account" 
 
 #Get inputs from deployment
 def get_vm_input(context, inputs):
     global event
     global ipadress
     global hostname
-    global requestid
-    global project
-    global owner
         
-    event         = str(inputs["__metadata"]["eventTopicId"])   #Provision or remove - Not used yet. 
+    event         = str(inputs["__metadata"]["eventTopicId"])   #Provision or remove. 
     ip_raw        = inputs["addresses"]                         #Raw IP input
     ipadress      = str(ip_raw[0])[2:-2]                        #Cleaned up IP
     hostname_raw  = inputs["resourceNames"]                     #Raw Hostname
@@ -27,18 +25,18 @@ def get_vm_input(context, inputs):
 def handler(context, inputs):
     get_vm_input(context, inputs)
     #Open session
-    session = winrm.Session('dc.cmplab.dk', auth=('username','password'))
+    session = winrm.Session(DNS_Server, auth=(Username,Password))
     
     #Check for provision
     result = event.startswith('compute.provision')
     if result == True :
-      dns_command = "dnscmd.exe dc.cmplab.dk /RecordAdd cmplab.dk "+hostname+" 10 A "+ipadress+""
+      dns_command = "dnscmd.exe "+DNS_Server+" /RecordAdd "+DNS_Domain+" "+hostname+" 10 A "+ipadress+""
       result = session.run_ps(dns_command)
       print(result.std_out)
 
     #Check for removal
     result = event.startswith('compute.removal')
     if result == True :
-      dns_command = "dnscmd /RecordDelete cmplab.dk "+hostname+" a /f"
+      dns_command = "dnscmd /RecordDelete "+DNS_Domain+" "+hostname+" a /f"
       result = session.run_ps(dns_command)
       print(result.std_out)
